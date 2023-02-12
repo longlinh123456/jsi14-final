2cdada5cff991dd821651c802897919c27055a45
Wather149 <108135900+Wather149@users.noreply.github.com>
2023-02-12 16:04:38 +0700

    chỉnh sửa giao diện Sign Up, Login, Dashboard

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,8 +8,24 @@ module.exports = {
 		extend: {
 			colors: {
 				textgrey: "#20262E",
+				textyellow: "yellow",
+				textblue: "#058fff",
 			}
 		},
+		borderWidth: {
+			DEFAULT: '1px',
+			'0': '0',
+			'0.5': '0.5px',
+			'1': '1px',
+			'2': '2px',
+			'3': '3px',
+			'4': '4px',
+			'6': '6px',
+			'8': '8px',
+		  },
+		  boxShadow: {
+			'input': '0px 5px 25px 0px rgb(0 0 0 / 40%)',
+		  },
 	},
 	plugins: [],
 }
